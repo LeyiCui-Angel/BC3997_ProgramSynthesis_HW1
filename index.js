cf75f8e4784b1b6ab7f0a9a178ad5f08a196a496
Angel Cui <69516825+LeyiCui-Angel@users.noreply.github.com>
2023-02-02 14:01:27 -0500

    Update to support multiple operations

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function synth(){
+    // Get inputs
     i = document.getElementById("in").value;
     o = document.getElementById("out").value;
     const iArray = i.split(",");
     const oArray = o.split(",");
+
+    // Check inputs
     if (iArray.length != oArray.length){
         document.getElementById("result").value = "Input/output format incorrect. Please check.";
         return;
@@ -12,6 +15,7 @@ function synth(){
         return;
     }
 
+    // Get all possible functions for the first input/output pair.
     r = firstRound(iArray[0], oArray[0]);
     newR = structuredClone(r);;
 
@@ -35,26 +39,40 @@ function synth(){
 }
 
 function firstRound(i, o){
+    // Operations: +, -, *, /
     // Check addition
     resultAR = null;
     if (eval(o - i) >= 0) {
         var ar = eval("o-i");
         resultAR = "+" + ar;
     }
-    // Check multiplication
-    var mr = eval("o/i");
-    resultMR = "*" + mr;
     // Check subtraction
     resultSR = null;
     if (eval(i - o) >= 0) {
         var sr = eval("i-o");
         resultSR = "-" + sr;
     }
+    // Check multiplication
+    resultMR = null;
+    if (Number.isInteger(o/i)) {
+        console.log("yes")
+        var mr = eval("o/i");
+        resultMR = "*" + mr;
+    }
+    else {
+        var mr = Math.round(eval("o/i"));
+        var mr1 = Math.round(eval("o-(mr*i)"));
+        resultMR = "*" + mr + "+" + "("+mr1+")";
+    }
     // Check integer division
     resultDR = null;
     if (Number.isInteger(i/o)) {
         var dr = eval("i/o");
         resultDR = "/" + dr;
+    } else if (!Math.round(eval(i/o)) == 0 && !Math.round(eval("i/o")) == 1){
+        var dr = Math.round(eval("i/o"));
+        var dr1 = Math.round(eval("i%o"));
+        resultDR = "/"+dr+"+"+dr1;
     }
 
     results = [resultAR, resultMR, resultSR, resultDR];
