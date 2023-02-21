3efff256be935c1bead7cc4d3c5880031ea78a0c
Angel Cui <69516825+LeyiCui-Angel@users.noreply.github.com>
2023-02-20 23:13:47 -0500

    HW2 Submission
    + equivalents elimination
    + prune if value does not move towards output from previous value that is closest to the output
    - changed from hard-coded method
    This closes #1 and #2

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function synth(){
         return;
     }
     var func = [];
-    document.getElementById("result").value = (compute(iArray, oArray, func, 0));
+    document.getElementById("result").value = (compute(iArray, oArray, func, 0,0));
 }
 
-function compute(input,output,func,count){
+function compute(input,output,func,count,betaIn){
     if (count > 2){
         return "Couldn't find a solution";
     }
@@ -27,7 +27,20 @@ function compute(input,output,func,count){
     if(func.length==0){
         for (const l of lang) {
             for (const b of ops) {
-                next.push(b+l);
+                var c=0;
+                for(let i=0; i<input.length; i++){
+                    var equa = input[i]+b+l;
+                    console.log("the equation: "+equa);
+                    console.log("get output: "+eval(equa));
+                    console.log("expected output: "+output[i]);
+                    if (eval(equa)==output[i]){
+                        c++;
+                        if (c==input.length){
+                            return b+l;
+                        }
+                    }
+                    next.push(b+l);
+                }
             }
         }
     }
@@ -35,27 +48,36 @@ function compute(input,output,func,count){
         for (const f of func) {
             for (const l of lang) {
                 for (const b of ops) {
-                    next.push(f+b+l);
+                    var c=0;
+                    for(let i=0; i<input.length; i++){
+                        var equa = input[i]+f+b+l;
+                        console.log("the equation: "+equa);
+                        console.log("get output: "+eval(equa));
+                        console.log("expected output: "+output[i]);
+                        if (eval(equa)==output[i]){
+                            c++;
+                            if (c==input.length){
+                                return f+b+l;
+                            }
+                        }
+                        else if (eval(equa) > output[i]){
+                            if ((eval(equa) - output[i]) < betaIn){
+                                betaIn = (eval(equa) - output[i]);
+                                next.push(f+b+l);
+                            }
+                        }
+                        else {
+                            if((output[i] - eval(equa)) < betaIn){
+                                betaIn = (output[i] - eval(equa));
+                                next.push(f+b+l);
+                            }
+                        }
+                    }
                 }
             }
         }
     }
     console.log("list to check: "+next);
-    for(let j=0; j<next.length; j++){
-        var c=0;
-        for(let i=0; i<input.length; i++){
-            var equa = input[i]+next[j];
-            console.log("the equation: "+equa);
-            console.log("get output: "+eval(equa));
-            console.log("expected output: "+output[i]);
-            if (eval(equa)==output[i]){
-                c++;
-                if (c==input.length){
-                    return next[j];
-                }
-            }
-        }
-    }
     count++;
-    return compute(input,output,next,count);
+    return compute(input,output,next,count,betaIn);
 }
